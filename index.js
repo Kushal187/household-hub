475b07879ca1bde449f2f1ae7b10634b1f057b2f
Kushal Pendekanti <pendekanti.s@northeastern.edu>
2026-02-17 21:35:20 -0500

    cleaned

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import "dotenv/config";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import choresRouter from "./routes/chores.js";
-import expensesRouter from "./routes/expenses.js";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
